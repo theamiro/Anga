6471dcd5845b2cbf5d9882de58c66406b9f4eab4
Michael Amiro <odindo.michael@gmail.com>
2021-05-12 08:10:53 +0300

    Initial Commit

---
+++ Anga/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Anga
+//
+//  Created by Michael Amiro on 12/05/2021.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
